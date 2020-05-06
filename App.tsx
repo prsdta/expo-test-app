694df34926e21d183c9fc8d341c5b33d280dc09c
prsdta -Aubin <uhryks@gmail.com>
2020-05-06 10:54:23 +0900

    feat: add state provider to the root

--- App.tsx
+++ App.tsx
@@ -2,17 +2,20 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import InputArea from "./src/components/InputArea";
 import { Provider as PaperProvider, Title } from "react-native-paper";
+import { AppStateProvider } from "./src/reducers/useAppState";
 
 export default function App() {
 	return (
-		<PaperProvider>
-			<View style={{ height: "100%", width: "100%" }}>
-				<View style={styles.container}>
-					<Title style={styles.title}>天気予報を見るためのアプリです。</Title>
-					<InputArea />
+		<AppStateProvider>
+			<PaperProvider>
+				<View style={{ height: "100%", width: "100%" }}>
+					<View style={styles.container}>
+						<Title style={styles.title}>天気予報を見るためのアプリです。</Title>
+						<InputArea />
+					</View>
 				</View>
-			</View>
-		</PaperProvider>
+			</PaperProvider>
+		</AppStateProvider>
 	);
 }
 
