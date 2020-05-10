15ead98e39e8890e1f93e9afc8aad765444fe953
prsdta -Aubin <uhryks@gmail.com>
2020-05-11 00:18:28 +0900

    chore: improve testing setup

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,9 @@
 // jest.config.js
 module.exports = {
 	verbose: true,
-	// taken from expo docs
-	preset: "jest-expo",
+	// FIXME Some selectors like getByText are broken, but neither preset expo nor
+	// react-native (from testing-librairy) seem to work.
+	preset: "react-native",
 	transformIgnorePatterns: [
 		"node_modules/(?!(jest-)?react-native|react-clone-referenced-element|@react-native-community|expo(nent)?|@expo(nent)?/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|@sentry/.*)"
 	]
